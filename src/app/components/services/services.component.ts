41c74c5963ee3f5ec5b04d2b472862a4f4125732
swarajgaidhane15 <rutvikgaidhane1508@gmail.com>
2020-10-21 13:32:42 +0530

    Added services page

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -9,7 +9,21 @@ export class ServicesComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  companies = [
+    'https://tppwebsolutions.com/wp-content/uploads/logo-demo3.png',
+    'https://lh3.googleusercontent.com/proxy/20fIlgP-5r2gmYjqB30SUigs8f76NPTJI_SD-p54526B32LTp71gzIbx6TcJm0M4k_rWdkmZy5iV94kCBuhtM4r0Q-y5queoh0yJlpSPoiXvz_xKAUg',
+    'https://images.template.net/wp-content/uploads/2017/04/Logo-Design1.jpg',
+    'https://corelpro.com/wp-content/uploads/2019/10/Logo-Design-Template.jpg',
+    'https://arenareviews.com/wp-content/uploads/2018/01/3d-company-logo-design.jpg',
+    'https://corelpro.com/wp-content/uploads/2019/03/Logo-Design-Template.jpg',
+    'https://tppwebsolutions.com/wp-content/uploads/logo-demo3.png',
+    'https://lh3.googleusercontent.com/proxy/20fIlgP-5r2gmYjqB30SUigs8f76NPTJI_SD-p54526B32LTp71gzIbx6TcJm0M4k_rWdkmZy5iV94kCBuhtM4r0Q-y5queoh0yJlpSPoiXvz_xKAUg',
+    'https://images.template.net/wp-content/uploads/2017/04/Logo-Design1.jpg',
+    'https://corelpro.com/wp-content/uploads/2019/10/Logo-Design-Template.jpg',
+    'https://arenareviews.com/wp-content/uploads/2018/01/3d-company-logo-design.jpg',
+    'https://corelpro.com/wp-content/uploads/2019/03/Logo-Design-Template.jpg',
+  ]
+
+  ngOnInit(): void {}
 
 }
